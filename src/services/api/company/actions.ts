4998c8e84d3c92621214655bea40d1f463422c7b
gustavosubi <134242156+gustavosubi@users.noreply.github.com>
2024-07-31 12:36:36 +1000

    Bugfix/sandbox environment (#104)
    * chore(log): http headers and baseurl
    
    * chore(release): changeset

--- src/services/api/company/actions.ts
+++ src/services/api/company/actions.ts
@@ -1,12 +1,18 @@
 import { constructAPIURL } from '..';
 import type { Company } from '../../../types/company';
 import axiosClient from '../../axios';
+import logger from '../../logger';
 import { COMPANY_URL } from './paths';
 
 /**
  * Gets the company that is attached to the authorisation headers.
  */
 export const getCompany = async (): Promise<Company> => {
+  logger.log('[API] GET /company', {
+    headers: axiosClient.defaults.headers,
+    baseURL: axiosClient.defaults.baseURL,
+  });
+
   const response = await axiosClient.get<Company>(constructAPIURL(COMPANY_URL));
   return response.data;
 };
