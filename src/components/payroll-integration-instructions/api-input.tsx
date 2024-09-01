388b083aa08dbae65cf87f337bac9e321116f653
Keegan Potgieter <keegan.pot7@gmail.com>
2024-09-01 12:56:59 +1000

    fix: form input logic and stories

--- src/components/payroll-integration-instructions/api-input.tsx
+++ src/components/payroll-integration-instructions/api-input.tsx
@@ -5,25 +5,13 @@ import React from 'react';
 type ApiKeyInputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 const ApiKeyInput = React.forwardRef<HTMLInputElement, ApiKeyInputProps>(
-  ({ value = '', onChange, className, ...props }, ref) => {
-    const [internalValue, setInternalValue] = React.useState(value);
+  ({ className, ...props }, ref) => {
     const [isHovered, setIsHovered] = React.useState(false);
-
-    const inputType: 'text' | 'password' = React.useMemo(() => {
-      return isHovered ? 'text' : 'password';
-    }, [isHovered]);
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setInternalValue(e.target.value);
-      onChange?.(e);
-    };
 
     return (
       <Input
         {...props}
-        type={inputType}
-        value={internalValue}
-        onChange={handleChange}
+        type={isHovered ? 'text' : 'password'}
         placeholder='Enter API Key'
         ref={ref}
         onMouseEnter={() => setIsHovered(true)}
--- src/components/payroll-integration-instructions/inputs.stories.tsx
+++ src/components/payroll-integration-instructions/inputs.stories.tsx
@@ -1,24 +1,32 @@
 import { Button } from '../../ui/button';
 import { Form } from '../../ui/form';
 import ApiKeyInput from './api-input';
+import DomainInput from './domain-input';
 import type { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
-const meta: Meta<typeof ApiKeyInput> = {
-  title: 'Form Components/ApiKeyInput',
-  component: ApiKeyInput,
+type PropsAndCustomArgs = React.ComponentProps<typeof Form> & {
+  domainContext: string;
+};
+
+const meta: Meta = {
+  title: 'Form Components/Inputs',
   tags: ['autodocs'],
+  component: Form,
+  subcomponents: {
+    ApiKeyInput: ApiKeyInput as React.ComponentType<unknown>,
+    DomainInput: DomainInput as React.ComponentType<unknown>,
+  },
   decorators: [
-    (): React.ReactElement => {
-      const form = useForm<{ apiKey: string }>();
+    (_, { args }): React.ReactElement => {
+      const form = useForm<{ apiKey: string; domain: string }>();
+      const { register, handleSubmit } = form;
       const [formData, setFormData] = React.useState<string | null>(null);
 
-      const handleOnSubmit = (data: { apiKey: string }) => {
+      const handleOnSubmit = (data: { apiKey: string; domain: string }) => {
         setFormData(JSON.stringify(data, null, 2));
       };
-
-      const { register, handleSubmit } = form;
 
       return (
         <div
@@ -35,10 +43,14 @@ const meta: Meta<typeof ApiKeyInput> = {
                 {...register('apiKey')}
                 id='search_subi-connect-payroll-integration-worflow_apiKey'
               />
+              <DomainInput
+                {...register('domain')}
+                domainContext={args.domainContext}
+                id='search_subi-connect-payroll-integration-worflow_domain'
+              />
               <Button type='submit' className='sc-mt-4'>
                 Finish
               </Button>
-
               {formData && (
                 <pre className='sc-mt-4 sc-rounded sc-bg-gray-100 sc-p-2'>
                   {formData}
@@ -53,8 +65,10 @@ const meta: Meta<typeof ApiKeyInput> = {
 };
 
 export default meta;
-type Story = StoryObj<typeof ApiKeyInput>;
+type Story = StoryObj<PropsAndCustomArgs>;
 
 export const Default: Story = {
-  name: 'Default',
+  args: {
+    domainContext: 'example.com',
+  },
 };
