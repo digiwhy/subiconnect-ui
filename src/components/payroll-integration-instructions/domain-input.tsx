a7db6815fb7c1d0da72b6fa71a4032d27def44a9
Keegan Potgieter <keegan.pot7@gmail.com>
2024-09-02 14:28:37 +1000

    fix: domain input ui/ux

--- src/components/payroll-integration-instructions/domain-input.tsx
+++ src/components/payroll-integration-instructions/domain-input.tsx
@@ -33,7 +33,7 @@ const DomainInput = React.forwardRef<HTMLInputElement, DomainInputProps>(
     };
 
     return (
-      <div className='sc-relative sc-mb-2 sc-flex sc-w-full sc-flex-col sc-items-start sc-gap-4 sc-overflow-clip'>
+      <div className='sc-relative sc-mb-2 sc-flex sc-w-full sc-flex-col sc-items-start sc-gap-4 sc-overflow-visible'>
         <Input
           {...props}
           type='text'
@@ -46,7 +46,7 @@ const DomainInput = React.forwardRef<HTMLInputElement, DomainInputProps>(
         />
         <div
           className={cn(
-            'sc-relative sc-right-2 sc-top-1/2 sc-ml-1 sc-flex sc-h-3/4 -sc-translate-y-1/2 sc-items-center sc-justify-center sc-bg-background sc-px-1 sc-text-xs sc-text-gray-500 sm:sc-absolute',
+            'sm:sc-max-w-1/2 sc-relative sc-right-0 sc-top-1/2 sc-flex sc-h-3/4 sc-max-w-full -sc-translate-y-1/2 sc-items-center sc-justify-start sc-overflow-x-scroll sc-bg-background sc-text-xs sc-text-gray-500 sm:sc-absolute sm:sc-right-2 sm:sc-pl-2',
             {
               'sc-hidden sm:sc-hidden': !subDomain || !domainContext,
             },
