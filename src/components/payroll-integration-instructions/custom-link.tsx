513fd2113809ce43f92db141fea7f1eac30eee46
Keegan Potgieter <keegan.pot7@gmail.com>
2024-09-02 17:16:20 +1000

    fix: add CustomLink to alter the default behaviour of 'a' tags in integration instructions

---
+++ src/components/payroll-integration-instructions/custom-link.tsx
@@ -0,0 +1,17 @@
+import type { TypedOmit } from '../../types/utils';
+import React from 'react';
+
+type CustomLinkProps = TypedOmit<
+  React.ComponentPropsWithoutRef<'a'>,
+  'rel' | 'target'
+>;
+
+const CustomLink: React.FC<CustomLinkProps> = ({ children, ...props }) => {
+  return (
+    <a {...props} rel='noopener noreferrer' target='_blank'>
+      {children}
+    </a>
+  );
+};
+
+export default CustomLink;
