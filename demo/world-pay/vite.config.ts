cae9a7944bab7bb083cabe856ad435403543b443
Keegan Potgieter <keegan.pot7@gmail.com>
2024-09-26 18:36:14 +1000

    chore (demos): add https to localhost

--- demo/world-pay/vite.config.ts
+++ demo/world-pay/vite.config.ts
@@ -1,6 +1,13 @@
 import react from '@vitejs/plugin-react-swc';
+import dotenv from 'dotenv';
+import os from 'os';
 import path from 'path';
 import { defineConfig } from 'vite';
+
+dotenv.config();
+
+const key = process.env.VITE_KEY;
+const cert = process.env.VITE_CERT;
 
 export default defineConfig({
   plugins: [react()],
@@ -14,5 +21,13 @@ export default defineConfig({
   },
   server: {
     port: 3001,
+    ...(key && cert
+      ? {
+          https: {
+            key: `${os.homedir()}/${key}`,
+            cert: `${os.homedir()}/${cert}`,
+          },
+        }
+      : {}),
   },
 });
