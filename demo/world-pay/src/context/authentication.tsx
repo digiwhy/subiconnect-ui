f2fbb4082e51586bb789629750faee22dcec4f04
Keegan Potgieter <keegan.pot7@gmail.com>
2024-09-27 16:36:44 +1000

    fix (demos): add dotenv and fix api key

--- demo/world-pay/src/context/authentication.tsx
+++ demo/world-pay/src/context/authentication.tsx
@@ -73,7 +73,7 @@ export const AuthenticationProvider = ({
 
       setApiKeyLocalStorage(value);
     },
-    [setApiKeyLocalStorage],
+    [setApiKeyLocalStorage, apiKey],
   );
 
   const handleSetApiKey = (value: string) => {
