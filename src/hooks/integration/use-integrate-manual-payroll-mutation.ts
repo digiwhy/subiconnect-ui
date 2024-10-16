a5ecc8c2e4f36a6c64d7067cc552f0e49fc5493f
Keegan Potgieter <keegan.pot7@gmail.com>
2024-10-16 15:44:08 +1100

    refactor: move query invalidation to mutation

--- src/hooks/integration/use-integrate-manual-payroll-mutation.ts
+++ src/hooks/integration/use-integrate-manual-payroll-mutation.ts
@@ -1,16 +1,56 @@
+import { BASE_COMPANY_QUERY_KEY } from '../use-company';
+import { BASE_PAYROLL_APPLICATION_QUERY_KEY } from '../use-payroll-systems';
 import { useSubiConnectContext } from '@/context/subi-connect';
 import { useSubiConnectMutation } from '@/hooks/use-subi-connect-query';
 import { integrateManualPayroll } from '@/services/api/payroll/actions';
 import type { UseIntegrateManualPayrollMutationProps } from '@/types/integration';
+import { SUBI_CONNECT_QUERY_KEY } from '@/types/main';
+import { useQueryClient } from '@tanstack/react-query';
 
 export const useIntegrateManualPayrollMutation = () => {
   const { connectionService } = useSubiConnectContext();
+  const queryClient = useQueryClient();
 
   return useSubiConnectMutation({
     mutationFn: ({ payrollName }: UseIntegrateManualPayrollMutationProps) => {
       return integrateManualPayroll(connectionService)({
         payrollName,
       });
+    },
+    onSuccess: (_, { payrollName }) => {
+      /**
+       * Invalidates the list and all details of the payroll application.
+       * Invalidates the company details.
+       */
+      Promise.all([
+        queryClient.invalidateQueries({
+          queryKey: [
+            SUBI_CONNECT_QUERY_KEY,
+            { context: connectionService.getContext() },
+            BASE_PAYROLL_APPLICATION_QUERY_KEY,
+            'list',
+          ],
+        }),
+
+        queryClient.invalidateQueries({
+          queryKey: [
+            SUBI_CONNECT_QUERY_KEY,
+            { context: connectionService.getContext() },
+            BASE_PAYROLL_APPLICATION_QUERY_KEY,
+            'detail',
+            `MANUAL (${payrollName})`,
+          ],
+        }),
+
+        queryClient.invalidateQueries({
+          queryKey: [
+            SUBI_CONNECT_QUERY_KEY,
+            { context: connectionService.getContext() },
+            BASE_COMPANY_QUERY_KEY,
+            'detail',
+          ],
+        }),
+      ]);
     },
   });
 };
