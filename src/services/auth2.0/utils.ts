2e1ed819e069774553bffe0a269168cf5fdcb38c
Keegan Potgieter <keegan.pot7@gmail.com>
2024-10-27 14:55:29 +1100

    feat: enhance OAuth2 integration and version bump to 5.0.0

--- src/services/auth2.0/utils.ts
+++ src/services/auth2.0/utils.ts
@@ -4,7 +4,7 @@ export const getAuthWindowOptions = () => {
   const left = window.innerWidth / 2 - width / 2;
   const top = window.innerHeight / 2 - height / 2;
 
-  return `popup, toolbar=no, location=no, directories=no, status=no, menubar=no,
+  return `popup, opener, toolbar=no, location=no, directories=no, status=no, menubar=no,
         scrollbars=no, copyhistory=no, width=${width},
         height=${height}, top=${top}, left=${left}`;
 };
